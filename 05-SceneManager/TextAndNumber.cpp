2c1605352c42e4a3de5a8218c653f4cf03640f84
soulofdevil <vothanhthuan153@gmail.com>
2023-05-26 10:27:25 +0700

    Add remaining sprite of Mario

---
+++ 05-SceneManager/TextAndNumber.cpp
@@ -0,0 +1,29 @@
+#include "TextAndNumber.h"
+
+void TextAndNumber::Render(float posX, float posY, string str)
+{
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			textSpr = CSprites::GetInstance()->Get(str[i] - '0' + 200000);
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			textSpr = CSprites::GetInstance()->Get(str[i] - 'A' + 200010);
+		else if (str[i] == '!')
+			textSpr = CSprites::GetInstance()->Get(200036);
+		else if (str[i] == ' ')
+			textSpr = CSprites::GetInstance()->Get(200037);
+		//textSpr = CSprites::GetInstance()->Get(str[i]-'a'+ 200010);
+		float x, y;
+		CGame::GetInstance()->GetCamPos(x, y);
+
+		textSpr->Draw(x + 100 + i * 25, y + 100);
+	}
+
+}
+
+string TextAndNumber::FillZeroString(string str, int MaxStringLenght)
+{
+	while (str.size() < MaxStringLenght)
+		str = "0" + str;
+	return str;
+}
